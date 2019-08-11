79a898fd0163e8b98374d03d4e528a49362494c6
gbldsz <1833220515@qq.com>
2019-08-11 17:03:24 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Author: liu deng
+ * @Date: 2019-08-11 17:00:46
+ * @LastEditTime: 2019-08-11 17:01:18
+ * @LastEditors: liu deng
+ */
+console.log('app-watch is start!');
