8a0ce5090b8b06eb80172b054f16a2ae86f95c6c
gbldsz <1833220515@qq.com>
2019-08-11 22:28:03 +0800

    hook XMLHttpRequest api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: liu deng
  * @Date: 2019-08-11 17:00:46
- * @LastEditTime: 2019-08-11 19:05:08
+ * @LastEditTime: 2019-08-11 22:18:28
  * @LastEditors: liu deng
  */
 class appWatch {
@@ -14,7 +14,10 @@ class appWatch {
     let that = this;
     that.reqList = {};
     // hook XMLHttpRequest.open api
-    that._open = window.XMLHttpRequest.prototype.open;
+    let _open = window.XMLHttpRequest.prototype.open;
+    let _send = window.XMLHttpRequest.prototype.send;
+    that._open = _open;
+    that._send = _send;
     window.XMLHttpRequest.prototype.open = function() {
       let XMLReq = this;
       let args = [].slice.call(arguments),
@@ -79,9 +82,9 @@ class appWatch {
           clearInterval(timer);
         }
 
-        if (!XMLReq._noVConsole) {
-          that.updateRequest(id, item);
-        }
+        that.updateRequest(id, item);
+        that.addDoneReqList(id);
+        console.log(item);
         return _onreadystatechange.apply(XMLReq, arguments);
       };
       XMLReq.onreadystatechange = onreadystatechange;
@@ -97,6 +100,48 @@ class appWatch {
       }, 10);
 
       return _open.apply(XMLReq, args);
+    };
+    // mock send()
+    window.XMLHttpRequest.prototype.send = function() {
+      let XMLReq = this;
+      let args = [].slice.call(arguments),
+        data = args[0];
+
+      let item = that.reqList[XMLReq._requestID] || {};
+      item.method = XMLReq._method.toUpperCase();
+
+      let query = XMLReq._url.split('?'); // a.php?b=c&d=?e => ['a.php', 'b=c&d=', '?e']
+      item.url = query.shift(); // => ['b=c&d=', '?e']
+
+      if (query.length > 0) {
+        item.getData = {};
+        query = query.join('?'); // => 'b=c&d=?e'
+        query = query.split('&'); // => ['b=c', 'd=?e']
+        for (let q of query) {
+          q = q.split('=');
+          item.getData[q[0]] = decodeURIComponent(q[1]);
+        }
+      }
+
+      if (item.method == 'POST') {
+        // save POST data
+        if (that.isString(data)) {
+          let arr = data.split('&');
+          item.postData = {};
+          for (let q of arr) {
+            q = q.split('=');
+            item.postData[q[0]] = q[1];
+          }
+        } else if (that.isPlainObject(data)) {
+          item.postData = data;
+        }
+      }
+
+      if (!XMLReq._noVConsole) {
+        that.updateRequest(XMLReq._requestID, item);
+      }
+
+      return _send.apply(XMLReq, args);
     };
   }
   /**
@@ -114,6 +159,55 @@ class appWatch {
     });
     return id;
   }
+  updateRequest(id, data) {
+    // see whether add new item into list
+    let preCount = Object.keys(this.reqList).length;
+
+    // update item
+    let item = this.reqList[id] || {};
+    for (let key in data) {
+      item[key] = data[key];
+    }
+    this.reqList[id] = item;
+  }
+  isPlainObject(obj) {
+    let hasOwn = Object.prototype.hasOwnProperty;
+    // Must be an Object.
+    if (!obj || typeof obj !== 'object' || obj.nodeType || isWindow(obj)) {
+      return false;
+    }
+    try {
+      if (
+        obj.constructor &&
+        !hasOwn.call(obj, 'constructor') &&
+        !hasOwn.call(obj.constructor.prototype, 'isPrototypeOf')
+      ) {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+    let key;
+    for (key in obj) {
+    }
+    return key === undefined || hasOwn.call(obj, key);
+  }
+  isString(value) {
+    return Object.prototype.toString.call(value) == '[object String]';
+  }
+  addDoneReqList(id) {
+    if (!Array.isArray(this.doneReqList)) {
+      this.doneReqList = [];
+    }
+    if (this.doneReqList[id]) {
+      return;
+    }
+    let currentReq = this.reqList[id];
+    if (currentReq.readyState === 4 && currentReq.status === 200) {
+      this.doneReqList.push(currentReq);
+      console.log(this.doneReqList);
+    }
+  }
 }
 
-export default appWatch;
+// export default appWatch;
